7b795c95603259c07e60f6a2080434369b7fd941
dessasilv <andressasilva1423ag@gmail.com>
2023-05-16 09:19:19 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+var b = parseInt(window.prompt ('escreva um número') );
+var a = parseInt(window.prompt ('escreva outro numero') );
+
+if(a==b){
+    document.write( "soma: ", a +b)
+} else {
+    document.write('multiplicação: ', a*b)
+}
