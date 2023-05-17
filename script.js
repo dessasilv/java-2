e2c36b29a29b7d2c66a4cbf09a2b09bf5916d69b
dessasilv <andressasilva1423ag@gmail.com>
2023-05-16 21:36:31 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var b = parseInt(window.prompt ('escreva um número') );
-var a = parseInt(window.prompt ('escreva outro numero') );
+var a = parseInt(window.prompt ('escreva outro numero') );//questão3
+
+var preco = parseInt(window.prompt('fale o preço de seu produto'));
+var prestaçao = parseInt(window.prompt('quantas prestações ira pagar'))
+var acrescimo1 = (preco * 0.10)
+var acrescimo2 = (preco * 0.20) //questoa 4
 
 if(a==b){
     document.write( "soma: ", a +b)
 } else {
-    document.write('multiplicação: ', a*b)
+    document.write('multiplicação: ', a*b, )
+}
+if(prestaçao<=3){
+    document.write(' o preço total a pagar será: ',preco + acrescimo1, ' e a parcela ficará: ', (preco + acrescimo1)/prestaçao )
+} else {
+    document.write(' o preço total será: ', acrescimo2 + preco, ' e a parcela ficará: ', (preco + acrescimo2)/prestaçao)
 }
