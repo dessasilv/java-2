0b10ea53079b9b2df45e9f6b4ea6a6c885c7fef2
dessasilv <andressasilva1423ag@gmail.com>
2023-05-18 08:15:03 -0300

    .

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var b = parseInt(window.prompt ('escreva um número') );
-var a = parseInt(window.prompt ('escreva outro numero') );//questão3
+var b = parseInt(window.prompt ('(questão 3) escreva um número') );
+var a = parseInt(window.prompt ('(questão 3) escreva outro numero') );//questão3
 
-var preco = parseInt(window.prompt('fale o preço de seu produto'));
-var prestaçao = parseInt(window.prompt('quantas prestações ira pagar'))
+var preco = parseInt(window.prompt('(questão 4) fale o preço de seu produto'));
+var prestaçao = parseInt(window.prompt('(questão 4) quantas prestações ira pagar'))
 var acrescimo1 = (preco * 0.10)
 var acrescimo2 = (preco * 0.20) //questoa 4
 
 if(a==b){
-    document.write( "soma: ", a +b)
+    document.write( "(questão 3) soma: ", a +b)
 } else {
-    document.write('multiplicação: ', a*b, )
+    document.write(' (questão 3) multiplicação: ', a*b, )
 }
-if(prestaçao<=3){
-    document.write(' o preço total a pagar será: ',preco + acrescimo1, ' e a parcela ficará: ', (preco + acrescimo1)/prestaçao )
-} else {
-    document.write(' o preço total será: ', acrescimo2 + preco, ' e a parcela ficará: ', (preco + acrescimo2)/prestaçao)
+if(prestaçao==1){
+    document.write('(questão 4) o preço total a pagar será: ',preco)
+} else if (prestaçao<=3){
+    document.write('(questão 4) o preço total a pagar será: ',preco + acrescimo1, ' e a parcela ficará: ', (preco + acrescimo1)/prestaçao )
+} else if (prestaçao>=4) {
+    document.write(' (questão4) o preço total será: ', acrescimo2 + preco, ' e a parcela ficará: ', (preco + acrescimo2)/prestaçao)
 }
+
+
